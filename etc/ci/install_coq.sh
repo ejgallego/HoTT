862d10dca1b0b5af1f53c2fbf0830bbb06d1e653
Emilio Jesus Gallego Arias <e+git@x80.org>
2017-05-14 01:50:18 +0200

    [travis] Simplify scripts.
    - always require autoreconf
    - always build coq from source
    - don't deploy autoconf generated files
    - don't update coq opam [obsolete]

--- etc/ci/install_coq.sh
+++ etc/ci/install_coq.sh
@@ -3,15 +3,6 @@
 PS4='$ '
 set -ex
 
-# if we're not testing the build of Coq, then install it from debian
-if [ -z "$BUILD_COQ" ]
-then
-    #echo | sudo add-apt-repository ppa:ezyang/coq-git
-    #sudo apt-get update -qq
-    sudo apt-get install -q coq libcoq-ocaml-dev
-    exit 0
-fi
-
 # in case we're run from out of git repo
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "$DIR" 1>/dev/null
@@ -27,17 +18,15 @@ then
     exit 1
 fi
 
-echo '$ git submodule sync'
+echo '$ git submodule sync + init'
 git submodule sync
-echo '$ git submodule update --init --recursive'
 git submodule update --init --recursive
 
+echo '$ checking out coq'
+git clone --depth=1 -b ${COQ_VERSION} https://github.com/coq/coq.git coq-HoTT
+
 pushd coq-HoTT
-if [ ! -z "$FORCE_COQ_VERSION" ]
-then
-    git checkout "$FORCE_COQ_VERSION" || exit $?
-fi
-NJOBS=1
+
 [ -e .opam ] || opam init -j ${NJOBS} --compiler=system -n -y
 eval $(opam config env)
 opam config var root
@@ -51,6 +40,7 @@ echo '$ sudo make install-binaries + rsync plugins theories'
 touch bin/coqtop.byte bin/coqchk stm/{proof,tac,query}workertop.cma
 sudo make install-binaries install-devfiles
 sudo rsync -a plugins theories /usr/local/lib/coq/
+
 popd
 
 popd 1>/dev/null
